bdca1e3aa29f09025c9133f71e470a9c6322bd26
u19900101 <815000342@qq.com>
2021-04-03 11:05:59 +0800

    双亲委派机制--利用 自定义一个String进行模拟

---
+++ src/java/lang/String.java
@@ -0,0 +1,20 @@
+package java.lang;
+
+/**
+ * @author lppppp
+ * @create 2021-04-03 10:55
+ */
+public class String {
+    /*static {
+        System.out.println("测试 创建了String类");
+    }*/
+
+    public static void main(String[] args) {
+        System.out.println("memeda");
+    }
+/*
+*   错误: 在类 java.lang.String 中找不到 main 方法, 请将 main 方法定义为:
+     public static void main(String[] args)
+    否则 JavaFX 应用程序类必须扩展javafx.application.Application
+* */
+}
