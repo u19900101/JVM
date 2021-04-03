3b3788bf419aacf746ff9c3bca033a00b3106626
u19900101 <815000342@qq.com>
2021-04-03 10:22:19 +0800

    init

---
+++ src/main/java/com/atguigu/java/chapter02/ClassLoaderTest2.java
@@ -0,0 +1,18 @@
+package com.atguigu.java.chapter02;
+
+/**
+ * @author lppppp
+ * @create 2021-04-03 10:11
+ * 获取ClassLoader的途径
+ */
+public class ClassLoaderTest2 {
+    public static void main(String[] args) throws ClassNotFoundException {
+        //1. 通过 Class.forName("java.lang.String").getClassLoader()
+        System.out.println(Class.forName("java.lang.String").getClassLoader());
+        System.out.println(Class.forName("com.atguigu.java.chapter02.ClassLoaderTest2").getClassLoader());
+
+    //    2.通过 获取当前线程上下文的ClassLoader：Thread.currentThread().getContextClassLoader()
+        ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+        System.out.println(contextClassLoader);
+    }
+}
