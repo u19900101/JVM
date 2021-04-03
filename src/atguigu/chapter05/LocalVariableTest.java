676aa3ba17f02c763a476e4552c9b148fffbec7d
u19900101 <815000342@qq.com>
2021-04-03 20:38:50 +0800

    局部变量表

---
+++ src/atguigu/chapter05/LocalVariableTest.java
@@ -0,0 +1,18 @@
+package atguigu.chapter05;
+
+/**
+ * @author lppppp
+ * @create 2021-04-03 20:31
+ */
+public class LocalVariableTest {
+    public static void main(String[] args) {
+        int i = 10;
+        LocalTest localTest = new LocalTest();
+        System.out.println(i);
+        return;
+    }
+}
+
+class LocalTest{
+
+}
