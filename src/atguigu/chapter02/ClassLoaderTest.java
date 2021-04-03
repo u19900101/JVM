23b1e76ab6efacc7af8b4e7f08fe3ffb91a05c9a
u19900101 <815000342@qq.com>
2021-04-03 11:05:21 +0800

    移动文件夹

---
+++ src/atguigu/chapter02/ClassLoaderTest.java
@@ -0,0 +1,29 @@
+package atguigu.chapter02;
+
+/**
+ * @author: 陌溪
+ * @create: 2020-07-05-9:47
+ */
+public class ClassLoaderTest {
+    public static void main(String[] args) {
+        // 获取系统类加载器
+        ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+        System.out.println("获取系统类加载器: "+systemClassLoader);
+
+        // 获取其上层的：扩展类加载器
+        ClassLoader extClassLoader = systemClassLoader.getParent();
+        System.out.println("获取其上层的：扩展类加载器 "+extClassLoader);
+
+        // 试图获取 根加载器
+        ClassLoader bootstrapClassLoader = extClassLoader.getParent();
+        System.out.println("试图获取 根加载器  "+bootstrapClassLoader);
+
+        // 获取自定义加载器
+        ClassLoader classLoader = ClassLoaderTest.class.getClassLoader();
+        System.out.println(" 获取自定义加载器   "+classLoader);
+
+        // 获取String类型的加载器
+        ClassLoader classLoader1 = String.class.getClassLoader();
+        System.out.println("获取String类型的加载器 "+classLoader1);
+    }
+}
---
+++ src/atguigu/chapter02/TwoParent.java
@@ -0,0 +1,11 @@
+package atguigu.chapter02;
+
+/**
+ * @author lppppp
+ * @create 2021-04-03 10:55
+ */
+public class TwoParent {
+    public static void main(String[] args) {
+        System.out.println("memeda");
+    }
+}
