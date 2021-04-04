5f58caca4d4be35ac7c7138d256256d538235f01
u19900101 <815000342@qq.com>
2021-04-04 17:34:08 +0800

    查看堆内存的内存分配情况

--- src/atguigu/chapter08/HeapSpaceInitial.java
+++ src/atguigu/chapter08/HeapSpaceInitial.java
@@ -12,6 +12,24 @@ package atguigu.chapter08;
  * - 初始内存大小：物理电脑内存大小/64
  *
  * - 最大内存大小：物理电脑内存大小/4
+ * 3.手动设置大小 -Xms500M -Xmx500M
+ *
+ * 4.查看堆内存的内存分配情况   jps  ->  jstat -gc 进程id
+ * 5. -XX:+PrintGCDetails
+ *
+ *
+ * -Xms:479M
+ * -Xmx:479M
+ * Heap
+ *  PSYoungGen      total 149504K, used 7711K [0x00000000f5980000, 0x0000000100000000, 0x0000000100000000)
+ *   eden space 128512K, 6% used [0x00000000f5980000,0x00000000f6107c70,0x00000000fd700000)
+ *   from space 20992K, 0% used [0x00000000feb80000,0x00000000feb80000,0x0000000100000000)
+ *   to   space 20992K, 0% used [0x00000000fd700000,0x00000000fd700000,0x00000000feb80000)
+ *  ParOldGen       total 341504K, used 0K [0x00000000e0c00000, 0x00000000f5980000, 0x00000000f5980000)
+ *   object space 341504K, 0% used [0x00000000e0c00000,0x00000000e0c00000,0x00000000f5980000)
+ *  Metaspace       used 3167K, capacity 4496K, committed 4864K, reserved 1056768K
+ *   class space    used 345K, capacity 388K, committed 512K, reserved 1048576K
+ *
  */
 public class HeapSpaceInitial {
     public static void main(String[] args) {
@@ -21,8 +39,14 @@ public class HeapSpaceInitial {
         long maxMemory = Runtime.getRuntime().maxMemory() / 1024 / 1024;
         System.out.println("-Xms:" + initialMemory + "M");
         System.out.println("-Xmx:" + maxMemory + "M");
-
-        System.out.println("系统内存大小为： "+initialMemory*64.0/1024+" G");
-        System.out.println("系统内存大小为： "+maxMemory*4.0/1024+" G");
+       /* try {
+            Thread.sleep(100000000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("end.......");
+        }*/
+        // System.out.println("系统内存大小为： "+initialMemory*64.0/1024+" G");
+        // System.out.println("系统内存大小为： "+maxMemory*4.0/1024+" G");
     }
 }
