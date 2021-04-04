33d76530c8e39fc6f16af5c1d843627b7fd85934
u19900101 <815000342@qq.com>
2021-04-04 17:14:59 +0800

    Xms 用来设置堆空间

--- src/atguigu/chapter08/HeapSpaceInitial.java
+++ src/atguigu/chapter08/HeapSpaceInitial.java
@@ -1,13 +1,17 @@
 package atguigu.chapter08;
 
 /**
+ * 1.
  * -Xms 用来设置堆空间（年轻代+老年代）的初始内存大小
  *  -X：是jvm运行参数
  *  ms：memory start
  * -Xmx：用来设置堆空间（年轻代+老年代）的最大内存大小
  *
- * @author: 陌溪
- * @create: 2020-07-06-20:44
+ * 2.默认情况下
+ *
+ * - 初始内存大小：物理电脑内存大小/64
+ *
+ * - 最大内存大小：物理电脑内存大小/4
  */
 public class HeapSpaceInitial {
     public static void main(String[] args) {
@@ -17,5 +21,8 @@ public class HeapSpaceInitial {
         long maxMemory = Runtime.getRuntime().maxMemory() / 1024 / 1024;
         System.out.println("-Xms:" + initialMemory + "M");
         System.out.println("-Xmx:" + maxMemory + "M");
+
+        System.out.println("系统内存大小为： "+initialMemory*64.0/1024+" G");
+        System.out.println("系统内存大小为： "+maxMemory*4.0/1024+" G");
     }
 }
