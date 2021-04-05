5688da848bc73bfed2b63bab6da12178a9f213ef
u19900101 <815000342@qq.com>
2021-04-05 20:47:33 +0800

    直接内存OOM

---
+++ src/atguigu/chapter10/BufferTest1.java
@@ -0,0 +1,30 @@
+package atguigu.chapter10;
+
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+
+/**
+ * @author lppppp
+ * @create 2021-04-05 20:42
+ */
+public class BufferTest1 {
+    private static final int BUFFER = 1024*1024*10;//10M
+
+    public static void main(String[] args) {
+        ArrayList<ByteBuffer> list = new ArrayList<>();
+        int count = 0;
+        try {
+            while (true){
+                ByteBuffer byteBuffer = ByteBuffer.allocate(BUFFER);
+                list.add(byteBuffer);
+                count++;
+            }
+        }finally {
+            System.out.println(count);
+        }
+    //Exception in thread "main" java.lang.OutOfMemoryError:
+        //                                                  Java heap space
+
+    }
+}
