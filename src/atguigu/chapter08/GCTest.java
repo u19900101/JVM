0959d52a596d377c7df166f692bda5d927ea4260
u19900101 <815000342@qq.com>
2021-04-05 09:22:50 +0800

    jvm的常用指令

--- src/atguigu/chapter08/GCTest.java
+++ src/atguigu/chapter08/GCTest.java
@@ -4,22 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * GC测试
+ 小结：堆空间的参数设置
  *
- * @author: 陌溪
- * @create: 2020-07-07-10:01
+ *  -XX：+PrintFlagsInitial：查看所有的参数的默认初始值
+ *  -XX：+PrintFlagsFinal：查看所有的参数的最终值（可能会存在修改，不再是初始值）
+ *          具体查看菜个参数的指令:
+ *          jps:查看当前运行中的进程
+ *          jinfo -flag SurvivorRatio  进程id
+ *  -Xms：初始堆空间内存（默认为物理内存的1/64）
+ *  -Xmx：最大堆空间内存（默认为物理内存的1/4）
+ *  -Xmn：设置新生代的大小。（初始值及最大值）
+ *  -XX:NewRatio：配置新生代与老年代在堆结构的占比
+ *
+ *  -XX:SurvivorRatio：设置新生代中Eden和S0/S1空间的比例
+ *  -XX:MaxTenuringThreshold：设置新生代垃圾的最大年龄
+ *  -XX：+PrintGCDetails：输出详细的GC处理日志
+ *    打印gc简要信息：①-Xx：+PrintGC  ② - verbose:gc
+ *  -XX:HandlePromotionFalilure：是否设置空间分配担保
  */
 public class GCTest {
     public static void main(String[] args) {
-        int i = 0;
+
         try {
-            List<String> list = new ArrayList<>();
-            String a = "mogu blog";
-            while(true) {
-                list.add(a);
-                a = a + a;
-                i++;
-            }
+            Thread.sleep(100000);
         }catch (Exception e) {
             e.getStackTrace();
         }
---
+++ src/atguigu/chapter08/YoungOldAreaTest.java
@@ -0,0 +1,16 @@
+package atguigu.chapter08;
+
+/**
+ * @author lppppp
+ * @create 2021-04-04 21:13
+ *
+ *
+ *
+ * -Xms60m -Xmx60m -XX:+PrintGCDetails
+ */
+public class YoungOldAreaTest {
+    public static void main(String[] args) throws InterruptedException {
+        byte[] bytes = new byte[20 * 1024 * 1024];//20M
+        // Thread.sleep(10000000);
+    }
+}
