d4878b3cda51f96a554a0de5e1cd7bd0b0c5e210
u19900101 <815000342@qq.com>
2021-04-07 09:28:22 +0800

    String str = new String("a") + new String("b") 创建了6个对象

--- src/atguigu/chapter13/StringNewTest.java
+++ src/atguigu/chapter13/StringNewTest.java
@@ -3,8 +3,16 @@ package atguigu.chapter13;
 /**
  * new String("ab") 会创建几个对象？ 看字节码就知道是2个对象
  *
- * @author: 陌溪
- * @create: 2020-07-11-11:17
+ * String str = new String("a") + new String("b");
+ * 我们创建了6个对象
+ *
+ * - 对象1：new StringBuilder()
+ * - 对象2：new String("a")
+ * - 对象3：常量池的 a
+ * - 对象4：new String("b")
+ * - 对象5：常量池的 b
+ * - 对象6：toString中会创建一个 new String("ab")
+ *   - 调用toString方法，不会在常量池中生成ab
  */
 public class StringNewTest {
     public static void main(String[] args) {
